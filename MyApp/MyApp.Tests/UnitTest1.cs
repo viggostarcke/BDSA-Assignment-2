4be462a342d2612f9eb276c92de173c66dbc0f9e
Mads <mroa@itu.dk>
2022-09-20 13:13:29 +0200

    initial studen test

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -1,10 +1,18 @@
 namespace MyApp.Tests;
 
-public class UnitTest1
+public class StudentTest
 {
     [Fact]
-    public void Test1()
+    public void Student_toString_returns_correct_output()
     {
-
+        //arrange
+            var expectedResult = "Id: 1, Name: Bob, Surname: TheBuilder, Status: Graduated";
+        //act
+            DateTime StartDate = DateTime.Parse("01/08/2018");
+            DateTime EndDate = DateTime.Parse("30/07/2021");
+            DateTime GraduationDate = DateTime.Parse("30/07/2021");
+            Student student = new Student(1, "Bob", "TheBuilder", StartDate, EndDate, GraduationDate);
+        //assert
+            student.ToString().Should().Be(expectedResult);
     }
 }
---
+++ MyApp/MyApp/Student.cs
@@ -0,0 +1,14 @@
+﻿var i = 1;
+public class Student {
+    public int Id { get; init; }
+    public string GivenName { get; set; }
+    public string Surname { get; set; }
+    public Status Status { get; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime GraduationDate { get; set; }
+
+
+}
+
+public enum Status{New, Active, Dropout, Graduated}
