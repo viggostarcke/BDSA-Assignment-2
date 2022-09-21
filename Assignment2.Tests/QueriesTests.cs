fd6645d7191e8334d6066e7664538cde6e67f646
Mads <mroa@itu.dk>
2022-09-21 23:36:22 +0200

    Queries almost done

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,63 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Query_Returns_4_wizards_By_Rowling()
     {
+        //Arrange
+        int expectedCount = 4;
+
+        //Act
+        var actualWizards = Queries.Get_All_Wizards_Name_By_Rowling_With_Linq();
+        var actualWizards2 = Queries.Get_All_Wizards_By_Rowling_With_Extensions();
+        
+        //Assert
+        actualWizards.Count().Should().Be(expectedCount);
+        actualWizards2.Count().Should().Be(expectedCount);
+    }
+
+    [Fact]
+    public void Query_Returns_1977_As_First_Year_Of_Sith() 
+    {
+        //Arrange
+        int expectedYear = 1977;
+        //Act
+        int? actualLinq = Queries.Get_First_Year_Of_Sith_Linq();
+        int? actualExtensions = Queries.Get_First_Year_Of_Sith_Extensions();
+        //Assert
+        actualLinq.Should().Be(expectedYear);
+        actualExtensions.Should().Be(expectedYear);
+    }
+
+    [Fact]
+    public void Query_Returns_Harry_Potter_Tuples() 
+    {
+        //Arrange
+        int expectedCount = 4;
+        HashSet<(string, int)> expectedWizards = new HashSet<(string, int)>();
+        expectedWizards.Add(("Harry Potter", 1997));
+        expectedWizards.Add(("Voldemort", 1997));
+        expectedWizards.Add(("Sirius Black", 1999));
+        expectedWizards.Add(("Severus Snape", 1997));
+        
+        //Act
+        var actualLinq = Queries.Get_Harry_Potter_Tuples_Linq();
+        var actualExtensions = Queries.Get_Harry_Potter_Tuples_Extensions();
+
+        //Assert
+        actualLinq.Count().Should().Be(expectedCount);
+        actualExtensions.Count().Should().Be(expectedCount);
+        actualLinq.Should().BeEquivalentTo(expectedWizards);
+        actualExtensions.Should().BeEquivalentTo(expectedWizards);
+    }
+
+    [Fact]
+    public void Query_Returns_Wizard_Grouped_By_Creator() 
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
 
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,62 @@ namespace Assignment2;
 
 public class Queries
 {
+    public static IEnumerable<string> Get_All_Wizards_Name_By_Rowling_With_Linq() 
+    {
+        var collection = WizardCollection.Create();
+        var wizardsOutput = from w in collection
+                            where w.Creator.Contains("Rowling")
+                            select w.Name;
+        return wizardsOutput;
+    }
+
+    public static IEnumerable<string> Get_All_Wizards_By_Rowling_With_Extensions() 
+    {
+        var wizardCollection = WizardCollection.Create();
+        var wizardsOutput = wizardCollection.Where(w => w.Creator.Contains("Rowling")).Select(w => w.Name);
+        return wizardsOutput;
+    }
+
+    public static int Get_First_Year_Of_Sith_Linq() 
+    {
+        var wizardCollection = WizardCollection.Create();
+        var output =    from w in wizardCollection
+                        where w.Name.StartsWith("Darth")
+                        select w.Year;
+        int min = (int) output.Min()!;
+        return min;
+    }
+
+    public static int? Get_First_Year_Of_Sith_Extensions() 
+    {
+        var wizardCollection = WizardCollection.Create();
+        int? min = (int) wizardCollection.Where(w => w.Name.StartsWith("Darth")).Select(w => w.Year).Min()!;
+        return min;
+    }
+
+    public static IEnumerable<(string, int?)> Get_Harry_Potter_Tuples_Linq() 
+    {
+        var wizardCollection = WizardCollection.Create();
+        var wizardsFromHarryPotter =    from w in wizardCollection
+                                        where w.Medium.StartsWith("Harry Potter")
+                                        select (w.Name, w.Year);
+        return wizardsFromHarryPotter;
+    }
+
+    public static IEnumerable<(string, int?)> Get_Harry_Potter_Tuples_Extensions() 
+    {
+        var wizardCollection = WizardCollection.Create();
+        var wizardsFromHarryPotter = wizardCollection.Where(w => w.Medium.StartsWith("Harry Potter")).Select(w => (w.Name, w.Year));
+        return wizardsFromHarryPotter;
+    }
+
+    public static IEnumerable<string> Get_Wizards_Grouped_By_Creator_Reverse_Order() 
+    {
+        var wizardCollection = WizardCollection.Create();
+
+        var wizardsOutput = from w in wizardCollection
+                            group w.Name by w.Creator into h
+                            select h;
+        throw new NotImplementedException();
+    }
 }
