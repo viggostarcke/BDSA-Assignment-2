a446b7dc340c0279fecfb6038749a515e45850e1
Mads <mroa@itu.dk>
2022-09-21 16:30:14 +0200

    extensions

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,38 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void IsSecure_Should_Return_True()
     {
+        //Arrange
+        bool expected = true;
+        Uri uri = new Uri("https://github.com/itu-bdsa");
+        //Act
+        bool actual = uri.IsSecure();
+        //Assert
+        actual.Should().Be(expected);
+    }
 
+    [Fact]
+    public void IsSecure_Should_Return_False()
+    {
+        //Arrange
+        bool expected = false;
+        Uri uri = new Uri("http://sport.tv2.dk/");
+        //Act
+        bool Actual = uri.IsSecure();
+        //Assert
+        Actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void WordCount_Should_Return_4()
+    {
+        //Arrange
+        int expected = 4;
+        string sentence = "Hello there, i'm Bob!";
+        //Act
+        int wordCount = sentence.WordCount();
+        //Assert
+        wordCount.Should().Be(expected);
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,7 @@ namespace Assignment2;
 
 public static class Extensions
 {
+    public static bool IsSecure(this Uri uri) => uri.ToString().StartsWith("https://");
+
+    public static int WordCount(this string str) => str.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
 }
